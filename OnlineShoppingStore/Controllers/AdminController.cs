b65d91c662b083300446042209b8ee0e525a8ebf
Lucian Rezende <lucianfiocello@gmail.com>
2021-01-20 19:11:49 -0300

    Add AdminStyle, AdminLayout, Dashboard, Categories

---
+++ OnlineShoppingStore/Controllers/AdminController.cs
@@ -0,0 +1,27 @@
+﻿using OnlineShoppingStore.DAL;
+using OnlineShoppingStore.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineShoppingStore.Controllers
+{
+    public class AdminController : Controller
+    {
+        public GenericUnitOfWork _unitOfWork = new GenericUnitOfWork();
+            
+        // GET: Admin
+        public ActionResult Dashboard()
+        {
+            return View();
+        }
+
+        public ActionResult Categories()
+        {
+            List<Tbl_Category> allCategories = _unitOfWork.GetRepositoryInstance<Tbl_Category>().GetAllRecordsIQueryable().Where(i => i.IsDelete == false).ToList();
+            return View(allCategories);
+        }
+    }
+}
